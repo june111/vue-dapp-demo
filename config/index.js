9f90095bb092f6f4ea8d29ae12441314dc65254a
June <june@linktimetech.com>
2019-01-07 19:48:44 +0800

    fix githubpage

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'vue-dapp-demo/',
+    assetsPublicPath: '/vue-dapp-demo/',
 
     /**
      * Source Maps
